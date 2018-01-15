820bde3f97f253e57573131025b695229b995437
Jimmi Holst Christensen <jhc@liab.dk>
2018-01-15 16:05:58 +0100

    WIP

--- crc.zig
+++ crc.zig
@@ -46,11 +46,13 @@ pub fn CrcSpec(comptime T: type) -> type {
         }
 
         pub fn next(comptime spec: &const Self, crc: T, byte: u8) -> T {
-            const table = comptime { 
-                @setEvalBranchQuota(256 * 8); 
-                spec.genTable();
-            };
-            return (crc >> 8) ^ table[@truncate(u8, crc ^ byte)];
+            // TODO: It seems, that for now we can't generate this table:
+            //       https://github.com/zig-lang/zig/issues/688
+            // const table = comptime {
+            //     @setEvalBranchQuota(256 * 8);
+            //     spec.genTable();
+            // };
+            return (crc >> 8) ^ spec.genTableEntry(@truncate(u8, crc ^ byte));
         }
 
         pub fn genTableEntry(comptime spec: &const Self, index: u8) -> T {
@@ -62,15 +64,15 @@ pub fn CrcSpec(comptime T: type) -> type {
 
             var i : u8 = 0;
             while (i < 8) : (i += 1) {
-                entry = if (entry & top_bit != 0) blk: {
-                    break :blk math.shl(T, entry, T(1));
+                entry = if (entry & 1 != 0) blk: {
+                    break :blk math.shr(T, entry, T(1)) ^ spec.polynomial;
                 } else blk: {
-                    break :blk math.shl(T, entry, T(1));
+                    break :blk math.shr(T, entry, T(1));
                 };
             }
 
             if (spec.reflect_input) entry = bits.reflect(T, entry);
-            return entry & spec.output_xor;
+            return entry ^ spec.output_xor;
         }
 
         pub fn genTable(comptime spec: &const Self) -> [256]T {
@@ -87,10 +89,13 @@ pub fn CrcSpec(comptime T: type) -> type {
 
 
 pub const crc32 = CrcSpec(u32) {
-    .polynomial     = 0b00000100110000010001110110110111,
+    .polynomial     = 0x04c11db7,
     .initial_value  = 0xFFFFFFFF,
-    .reflect_input  = true,
-    .reflect_output = true,
+    .reflect_input  = false,
+    .reflect_output = false,
     .output_xor     = 0xFFFFFFFF,
 };
-comptime { assert(crc32.done(crc32.calc("CRC-32")) == 0xCBF43926); }
+
+test "crc.crc32" {
+    assert(crc32.done(crc32.calc("CRC-32/BZIP2")) == 0xfc891918);
+}
