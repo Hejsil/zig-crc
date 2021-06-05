db5a1d3509566bd22125371e64d592fc1d8e2a99
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-06-05 17:59:24 +0200

    Update to 0.8.0

--- crc.zig
+++ crc.zig
@@ -20,8 +20,8 @@ fn reflect(comptime UInt: type, data: UInt) UInt {
 }
 
 test "crc.reflect" {
-    testing.expectEqual(@as(u8, 0b10000000), reflect(u8, 0b00000001));
-    testing.expectEqual(@as(u8, 0b00000001), reflect(u8, 0b10000000));
+    try testing.expectEqual(@as(u8, 0b10000000), reflect(u8, 0b00000001));
+    try testing.expectEqual(@as(u8, 0b00000001), reflect(u8, 0b10000000));
 }
 
 pub const crcspec_init_backward_cycles = 20000;
@@ -43,11 +43,11 @@ pub fn CrcSpec(comptime UInt: type) type {
         polynomial: UInt,
         initial_value: UInt,
         xor_value: UInt,
-        reflect_data: Reflect.Data,
-        reflect_remainder: Reflect.Remainder,
-        table: [256]UInt,
+        reflect_data: bool,
+        reflect_remainder: bool,
+        table: [256]UInt = undefined,
 
-        pub fn init(polynomial: UInt, initial_value: UInt, xor_value: UInt, reflect_data: Reflect.Data, reflect_remainder: Reflect.Remainder) CrcSpec(UInt) {
+        pub fn init(polynomial: UInt, initial_value: UInt, xor_value: UInt, reflect_data: bool, reflect_remainder: bool) CrcSpec(UInt) {
             var res = @This(){
                 .polynomial = polynomial,
                 .initial_value = initial_value,
@@ -113,55 +113,52 @@ pub fn Crc(comptime UInt: type) type {
         }
 
         pub fn update(crc: *@This(), bytes: []const u8) void {
-            const reflect_data = crc.spec.reflect_data == Reflect.Data.True;
-
             for (bytes) |byte| {
-                const entry = reflect_if(u8, reflect_data, byte) ^ (crc.remainder >> (bits - 8));
+                const entry = reflect_if(u8, crc.spec.reflect_data, byte) ^ (crc.remainder >> (bits - 8));
                 crc.remainder = crc.spec.table[entry] ^ math.shl(UInt, crc.remainder, @as(UInt, 8));
             }
         }
 
         pub fn final(crc: @This()) UInt {
-            const reflect_remainder = crc.spec.reflect_remainder == Reflect.Remainder.True;
-            const reflected = reflect_if(UInt, reflect_remainder, crc.remainder);
+            const reflected = reflect_if(UInt, crc.spec.reflect_remainder, crc.remainder);
             return reflected ^ crc.spec.xor_value;
         }
     };
 }
 
 // Specs below gotten from http://reveng.sourceforge.net/crc-catalogue/all.htm
-pub const crc8 = comptime blk: {
+pub const crc8 = blk: {
     @setEvalBranchQuota(crcspec_init_backward_cycles);
-    break :blk CrcSpec(u8).init(0x07, 0x00, 0x00, Reflect.Data.False, Reflect.Remainder.False);
+    break :blk CrcSpec(u8).init(0x07, 0x00, 0x00, false, false);
 };
 
 test "crc.crc8" {
-    testing.expectEqual(@as(u8, 0xF4), crc8.checksum("123456789"));
+    try testing.expectEqual(@as(u8, 0xF4), crc8.checksum("123456789"));
 }
 
-pub const crc16 = comptime blk: {
+pub const crc16 = blk: {
     @setEvalBranchQuota(crcspec_init_backward_cycles);
-    break :blk CrcSpec(u16).init(0x8005, 0x0000, 0x0000, Reflect.Data.True, Reflect.Remainder.True);
+    break :blk CrcSpec(u16).init(0x8005, 0x0000, 0x0000, true, true);
 };
 
 test "crc.crc16" {
-    testing.expectEqual(@as(u16, 0xBB3D), crc16.checksum("123456789"));
+    try testing.expectEqual(@as(u16, 0xBB3D), crc16.checksum("123456789"));
 }
 
-pub const crc32 = comptime blk: {
+pub const crc32 = blk: {
     @setEvalBranchQuota(crcspec_init_backward_cycles);
-    break :blk CrcSpec(u32).init(0x04C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, Reflect.Data.True, Reflect.Remainder.True);
+    break :blk CrcSpec(u32).init(0x04C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, true, true);
 };
 
 test "crc.crc32" {
-    testing.expectEqual(@as(u32, 0xCBF43926), crc32.checksum("123456789"));
+    try testing.expectEqual(@as(u32, 0xCBF43926), crc32.checksum("123456789"));
 }
 
-pub const crc64 = comptime blk: {
+pub const crc64 = blk: {
     @setEvalBranchQuota(crcspec_init_backward_cycles);
-    break :blk CrcSpec(u64).init(0x42F0E1EBA9EA3693, 0x0000000000000000, 0x0000000000000000, Reflect.Data.False, Reflect.Remainder.False);
+    break :blk CrcSpec(u64).init(0x42F0E1EBA9EA3693, 0x0000000000000000, 0x0000000000000000, false, false);
 };
 
 test "crc.crc64" {
-    testing.expectEqual(@as(u64, 0x6C40DF5F0B497347), crc64.checksum("123456789"));
+    try testing.expectEqual(@as(u64, 0x6C40DF5F0B497347), crc64.checksum("123456789"));
 }
