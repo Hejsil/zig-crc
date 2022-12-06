e6972650d0fa2eb44c7d1dd95aac913672bea3c5
Jimmi Holst Christensen <jhc@dismail.de>
2022-12-06 20:23:54 +0100

    Modernize api

--- crc.zig
+++ crc.zig
@@ -19,26 +19,18 @@ fn reflect(comptime UInt: type, data: UInt) UInt {
     return res;
 }
 
-test "crc.reflect" {
+fn reflectIf(comptime K: type, predicate: bool, data: K) K {
+    return if (predicate) reflect(K, data) else data;
+}
+
+test "reflect" {
     try testing.expectEqual(@as(u8, 0b10000000), reflect(u8, 0b00000001));
     try testing.expectEqual(@as(u8, 0b00000001), reflect(u8, 0b10000000));
 }
 
 pub const crcspec_init_backward_cycles = 20000;
 
-pub const Reflect = struct {
-    pub const Data = enum {
-        True,
-        False,
-    };
-
-    pub const Remainder = enum {
-        True,
-        False,
-    };
-};
-
-pub fn CrcSpec(comptime UInt: type) type {
+pub fn Spec(comptime UInt: type) type {
     return struct {
         polynomial: UInt,
         initial_value: UInt,
@@ -47,16 +39,8 @@ pub fn CrcSpec(comptime UInt: type) type {
         reflect_remainder: bool,
         table: [256]UInt = undefined,
 
-        pub fn init(polynomial: UInt, initial_value: UInt, xor_value: UInt, reflect_data: bool, reflect_remainder: bool) CrcSpec(UInt) {
-            var res = @This(){
-                .polynomial = polynomial,
-                .initial_value = initial_value,
-                .xor_value = xor_value,
-                .reflect_data = reflect_data,
-                .reflect_remainder = reflect_remainder,
-                .table = undefined,
-            };
-
+        pub fn init(spec: @This()) @This() {
+            var res = spec;
             const bits = @typeInfo(UInt).Int.bits;
             const top_bit = @as(UInt, 1) << (bits - 1);
 
@@ -66,7 +50,7 @@ pub fn CrcSpec(comptime UInt: type) type {
                 var bit: usize = 0;
                 while (bit < 8) : (bit += 1) {
                     if (crc & top_bit != 0) {
-                        crc = math.shl(UInt, crc, @as(UInt, 1)) ^ polynomial;
+                        crc = math.shl(UInt, crc, @as(UInt, 1)) ^ res.polynomial;
                     } else {
                         crc = math.shl(UInt, crc, @as(UInt, 1));
                     }
@@ -94,33 +78,26 @@ pub fn Crc(comptime UInt: type) type {
     return struct {
         const bits = @typeInfo(UInt).Int.bits;
 
-        spec: CrcSpec(UInt),
+        spec: Spec(UInt),
         remainder: UInt,
 
-        pub fn init(spec: CrcSpec(UInt)) @This() {
+        pub fn init(spec: Spec(UInt)) @This() {
             return @This(){
                 .spec = spec,
                 .remainder = spec.initial_value,
             };
         }
 
-        fn reflect_if(comptime K: type, ref: bool, data: K) K {
-            if (ref) {
-                return reflect(K, data);
-            } else {
-                return data;
-            }
-        }
-
         pub fn update(crc: *@This(), bytes: []const u8) void {
             for (bytes) |byte| {
-                const entry = reflect_if(u8, crc.spec.reflect_data, byte) ^ (crc.remainder >> (bits - 8));
+                const reflected_byte = reflectIf(u8, crc.spec.reflect_data, byte);
+                const entry = reflected_byte ^ (crc.remainder >> (bits - 8));
                 crc.remainder = crc.spec.table[entry] ^ math.shl(UInt, crc.remainder, @as(UInt, 8));
             }
         }
 
         pub fn final(crc: @This()) UInt {
-            const reflected = reflect_if(UInt, crc.spec.reflect_remainder, crc.remainder);
+            const reflected = reflectIf(UInt, crc.spec.reflect_remainder, crc.remainder);
             return reflected ^ crc.spec.xor_value;
         }
     };
@@ -129,36 +106,60 @@ pub fn Crc(comptime UInt: type) type {
 // Specs below gotten from http://reveng.sourceforge.net/crc-catalogue/all.htm
 pub const crc8 = blk: {
     @setEvalBranchQuota(crcspec_init_backward_cycles);
-    break :blk CrcSpec(u8).init(0x07, 0x00, 0x00, false, false);
+    break :blk Spec(u8).init(.{
+        .polynomial = 0x07,
+        .initial_value = 0x00,
+        .xor_value = 0x00,
+        .reflect_data = false,
+        .reflect_remainder = false,
+    });
 };
 
-test "crc.crc8" {
+test "crc8" {
     try testing.expectEqual(@as(u8, 0xF4), crc8.checksum("123456789"));
 }
 
 pub const crc16 = blk: {
     @setEvalBranchQuota(crcspec_init_backward_cycles);
-    break :blk CrcSpec(u16).init(0x8005, 0x0000, 0x0000, true, true);
+    break :blk Spec(u16).init(.{
+        .polynomial = 0x8005,
+        .initial_value = 0x0000,
+        .xor_value = 0x0000,
+        .reflect_data = true,
+        .reflect_remainder = true,
+    });
 };
 
-test "crc.crc16" {
+test "crc16" {
     try testing.expectEqual(@as(u16, 0xBB3D), crc16.checksum("123456789"));
 }
 
 pub const crc32 = blk: {
     @setEvalBranchQuota(crcspec_init_backward_cycles);
-    break :blk CrcSpec(u32).init(0x04C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, true, true);
+    break :blk Spec(u32).init(.{
+        .polynomial = 0x04C11DB7,
+        .initial_value = 0xFFFFFFFF,
+        .xor_value = 0xFFFFFFFF,
+        .reflect_data = true,
+        .reflect_remainder = true,
+    });
 };
 
-test "crc.crc32" {
+test "crc32" {
     try testing.expectEqual(@as(u32, 0xCBF43926), crc32.checksum("123456789"));
 }
 
 pub const crc64 = blk: {
     @setEvalBranchQuota(crcspec_init_backward_cycles);
-    break :blk CrcSpec(u64).init(0x42F0E1EBA9EA3693, 0x0000000000000000, 0x0000000000000000, false, false);
+    break :blk Spec(u64).init(.{
+        .polynomial = 0x42F0E1EBA9EA3693,
+        .initial_value = 0x0000000000000000,
+        .xor_value = 0x0000000000000000,
+        .reflect_data = false,
+        .reflect_remainder = false,
+    });
 };
 
-test "crc.crc64" {
+test "crc64" {
     try testing.expectEqual(@as(u64, 0x6C40DF5F0B497347), crc64.checksum("123456789"));
 }
